a865c6395ae21cd40dc4a53a6a2fe558a10c799c
sistlapriya <sistlasaiharipriya@gmail.com>
2026-03-05 09:55:09 +0530

    UC8: Linked List Based Palindrome Checker

--- APP/src/PalindromeCheckApp.java
+++ APP/src/PalindromeCheckApp.java
@@ -3,54 +3,96 @@ import java.util.Stack;
 import java.util.LinkedList;
 
 public class PalindromeCheckApp {
-    public static void main(String[] args) {
-        String input = "racecar";
 
-        // Using Queue (FIFO - First In First Out)
-        Queue<Character> queue = new LinkedList<>();
-        for (char c : input.toCharArray()) {
-            queue.add(c);
-        }
+        static class Node {
+            char data;
+            Node next;
 
-        // Using Stack (LIFO - Last In First Out)
-        Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        System.out.println("Input: " + input);
-        System.out.println("\nQueue (FIFO) Output:");
-        while (!queue.isEmpty()) {
-            System.out.print(queue.poll());
-        }
-
-        System.out.println("\nStack (LIFO) Output:");
-        while (!stack.isEmpty()) {
-            System.out.print(stack.pop());
-        }
-
-        // Compare outputs for palindrome validation
-        System.out.println("\n\nPalindrome Check:");
-        Queue<Character> queueCheck = new LinkedList<>();
-        Stack<Character> stackCheck = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-            queueCheck.add(c);
-            stackCheck.push(c);
-        }
-
-        boolean isPalindrome = true;
-        while (!queueCheck.isEmpty() && !stackCheck.isEmpty()) {
-            if (queueCheck.poll() != stackCheck.pop()) {
-                isPalindrome = false;
-                break;
+            Node(char data) {
+                this.data = data;
+                this.next = null;
             }
         }
 
-        if (isPalindrome) {
-            System.out.println(input + " is a palindrome (Queue and Stack outputs match in reverse)");
-        } else {
-            System.out.println(input + " is not a palindrome");
+        public static Node createLinkedList(String str) {
+            Node head = null, tail = null;
+
+            for (int i = 0; i < str.length(); i++) {
+                Node newNode = new Node(str.charAt(i));
+
+                if (head == null) {
+                    head = newNode;
+                    tail = newNode;
+                } else {
+                    tail.next = newNode;
+                    tail = newNode;
+                }
+            }
+            return head;
+        }
+
+        public static Node reverse(Node head) {
+            Node prev = null;
+            Node current = head;
+            Node next;
+
+            while (current != null) {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
+
+        public static boolean isPalindrome(Node head) {
+
+            if (head == null || head.next == null)
+                return true;
+
+            Node slow = head;
+            Node fast = head;
+
+            // Find middle of linked list
+            while (fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            Node secondHalf = reverse(slow.next);
+
+            Node firstHalf = head;
+            Node temp = secondHalf;
+
+            while (temp != null) {
+                if (firstHalf.data != temp.data)
+                    return false;
+
+                firstHalf = firstHalf.next;
+                temp = temp.next;
+            }
+
+            return true;
+        }
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter a string: ");
+            String input = sc.nextLine();
+
+            Node head = createLinkedList(input);
+
+            if (isPalindrome(head))
+                System.out.println("Palindrome");
+            else
+                System.out.println("Not a Palindrome");
+
+            sc.close();
         }
     }
-}
+
+
+
